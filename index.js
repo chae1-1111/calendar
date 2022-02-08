035388ab4b72068a5d359f740f5a33799bca24bc
chae1-1111 <dla4212@naver.com>
2022-02-08 21:26:44 +0900

    test99

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(logger("dev"));
 
 const testRouter = require("./router/testRouter");
