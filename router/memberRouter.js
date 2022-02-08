16e312e9ec485b2ad1c6c45c5795e97e4f1b6330
chae1-1111 <dla4212@naver.com>
2022-02-08 19:13:11 +0900

    test22

--- router/memberRouter.js
+++ router/memberRouter.js
@@ -33,7 +33,9 @@ memberRouter.route("/").get((req, res) => {
 });
 
 memberRouter.route("/").post((req, res) => {
-    console.log(req);
+    console.log(req.body);
+    console.log(req.params);
+    console.log(req.query);
     if (!auth(req.body.apikey)) {
         res.status(401).json({ code: 401, message: "Invalid API key" });
         return;
